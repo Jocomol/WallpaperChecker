ecd024303e656ad1388e08ef42526f7d7fcd7d5b
Jocomol <joelmeier08@gmail.com>
2021-04-13 16:24:24 +0200

    - Textboxes now functional

--- wallpaperchecker.py
+++ wallpaperchecker.py
@@ -4,62 +4,97 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from os.path import isfile, join, abspath
 import argparse
 import pygame
-
-parser = argparse.ArgumentParser(description="WIP")
-parser.add_argument("path", metavar="path", type=str, nargs="+", help="path to pictures") 
+from PIL import Image
 
 
-class Image:
+parser = argparse.ArgumentParser(description="WIP") ##TODO Description
+parser.add_argument("path", metavar="path", type=str, nargs="+", help="path to directory containting pictures") 
+
+class Text:
+    
+    def __init__(self, text):
+        self.text = text
+        self.width = 0
+        self.height = 0
+    
+    def setFont(self, font):
+        self.font = font
+        self.width, self.height = self.font.size(self.text)
+    
+    def setYCord(self, y_cord):
+        self.y_cord = y_cord   
+    
+    def render(self, display):
+        display.blit(self.font.render(self.text, True, (255, 255, 255)),(5,5 + self.y_cord))
+
+class Picture:
 
     def __init__(self, path, width, height):
         self.path = path
         self.width = width
         self.height = height
         self.image = pygame.transform.scale(pygame.image.load(self.path), (self.width, self.height))
-        
+        self.texts = self.textFactory(path)
+    
     def draw(self, display, font):
         display.fill((0,0,0))
         pygame.display.set_caption(self.path)
-        display.blit(self.image, (0, 0))    
-        self.text_width, self.text_height = font.size(self.path)
-        pygame.draw.rect(display, (0,0,0), pygame.Rect(0, 0, self.text_width + 10, self.text_height + 10))
-        display.blit(font.render(self.path, True, (255, 255, 255)),(5,5))
-    
+        display.blit(self.image, (0, 0))
+        self.renderText(font, display)
+
     def delete(self):
         remove(self.path)
         print("file",self.path,"deleted")
+    
+    def textFactory(self, path):
+        img = Image.open(path)     
+        texts = []
+        texts.append(Text(img.format))
+        texts.append(Text(str(img.width) + "x" + str(img.height))) #TODO Aspect Ratio with corresponding color
+        texts.append(Text(img.mode))
+        return texts
 
-def loadImages(absDirectoryPath, width, height):
-    images= []
-    for item in listdir(absDirectoryPath):
+    def renderText(self, font, display):
+        max_width = 0
+        full_height = 0
+        for text in self.texts:
+            text.setFont(font)
+            text.setYCord(full_height)
+            full_height += text.height
+            if text.width > max_width:
+                max_width = text.width
+        pygame.draw.rect(display, (0,0,0), pygame.Rect(0, 0, max_width + 10, full_height + 10))
+        for text in self.texts:
+            text.render(display)
+
+def loadPictures(absDirectoryPath, width, height):
+    pictures = []
+    for item in listdir(absDirectoryPath): ##TODO Support * and list of images
         filePath = join(absDirectoryPath, item)
         if isfile(filePath):
-            images.append(Image(filePath, width, height))
-    return images
+            pictures.append(Picture(filePath, width, height))
+    return pictures
 
-def showImages(display, font, images, maxIndex):
+def showPictures(display, font, pictures):
     running = True
-    images = iter(images)
-    image = next(images)
+    pictures = iter(pictures)
+    picture = next(pictures)
     while running:
         try:
-            image.draw(display, font)
+            picture.draw(display, font)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
-                    pygame.quit()
-                    quit()                    
+                    running = False
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_y:
-                    image = next(images)
+                    picture = next(pictures)
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_n:
-                    image.delete()
-                    image = next(images)
+                    picture.delete()
+                    picture = next(pictures)
                 pygame.display.update()
         except StopIteration:
             running = False
     else:
         pygame.quit()
-        quit()
-
 
 def main():
     args = parser.parse_args()
@@ -69,8 +104,8 @@ def main():
     display_height = 1080
     font = pygame.font.SysFont(None, 40)
     display = pygame.display.set_mode((display_width,display_height))
-    images = loadImages(abspath(args.path[0]), display_width, display_height)
-    showImages(display, font, images, len(images) -1)
+    pictures = loadPictures(abspath(args.path[0]), display_width, display_height)
+    showPictures(display, font, pictures)
 
 if __name__ == "__main__":
     main()
