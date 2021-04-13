abf6446aecb7f9739a23d1821246646cfb815115
Jocomol <joelmeier08@gmail.com>
2021-04-13 18:41:03 +0200

    Checked with flake8 wallpaperchecker.py --ignore=E501,E402 Added an if to prevent a crash when encountering second level directories

--- wallpaperchecker.py
+++ wallpaperchecker.py
@@ -7,26 +7,27 @@ import pygame
 from PIL import Image
 
 
-parser = argparse.ArgumentParser(description="WIP") ##TODO Description
-parser.add_argument("path", metavar="path", type=str, nargs="+", help="path to directory containting pictures") 
+parser = argparse.ArgumentParser(description="WIP")  # TODO Description
+parser.add_argument("path", metavar="path", type=str, nargs="+", help="path to directory containting pictures")
+
 
 class Text:
-    
-    def __init__(self, text, color=(255,255,255)):
+    def __init__(self, text, color=(255, 255, 255)):
         self.text = text
         self.width = 0
         self.height = 0
         self.color = color
-    
+
     def setFont(self, font):
         self.font = font
         self.width, self.height = self.font.size(self.text)
-    
+
     def setYCord(self, y_cord):
-        self.y_cord = y_cord   
-    
+        self.y_cord = y_cord
+
     def render(self, display):
-        display.blit(self.font.render(self.text, True, self.color),(5,5 + self.y_cord))
+        display.blit(self.font.render(self.text, True, self.color), (5, 5 + self.y_cord))
+
 
 class Picture:
     def __init__(self, path, width, height, max_index, index):
@@ -35,34 +36,33 @@ class Picture:
         self.height = height
         self.image = pygame.transform.scale(pygame.image.load(self.path), (self.width, self.height))
         self.texts = self.textFactory(self.path, max_index, index)
-         
+
     def draw(self, display, font):
-        display.fill((0,0,0))
+        display.fill((0, 0, 0))
         pygame.display.set_caption(self.path)
         display.blit(self.image, (0, 0))
         self.renderText(font, display)
 
     def delete(self):
         remove(self.path)
-        print("file",self.path,"deleted")
-    
+        print("file", self.path, "deleted")
+
     def textFactory(self, path, max_index, index):
         texts = []
-        img = Image.open(path)     
+        img = Image.open(path)
         color = self.calAspectRatio(img)
         texts.append(Text(img.format))
-        texts.append(Text(str(img.width) + "x" + str(img.height),color))
+        texts.append(Text(str(img.width) + "x" + str(img.height), color))
         texts.append(Text(img.mode))
         texts.append(Text(str(index) + "/" + str(max_index)))
         return texts
-    
-    def calAspectRatio(self, img):
-        ratio_difference = abs(16/9 - img.width / img.height)
-        if ratio_difference <= (2.5 * 0.18):
-            return (0,255,0)
-        else:
-            return (255,0,0)
 
+    def calAspectRatio(self, img):
+        ratio_difference = abs(16 / 9 - img.width / img.height)
+        if ratio_difference <= (2.5 * 0.18):
+            return (0, 255, 0)
+        else:
+            return (255, 0, 0)
 
     def renderText(self, font, display):
         max_width = 0
@@ -73,9 +73,10 @@ class Picture:
             full_height += text.height
             if text.width > max_width:
                 max_width = text.width
-        pygame.draw.rect(display, (0,0,0), pygame.Rect(0, 0, max_width + 10, full_height + 10))
+        pygame.draw.rect(display, (0, 0, 0), pygame.Rect(0, 0, max_width + 10, full_height + 10))
         for text in self.texts:
             text.render(display)
+
 
 def loadPictures(paths, width, height):
     pictures = []
@@ -90,8 +91,10 @@ def loadPictures(paths, width, height):
         elif isfile(path):
             picture_paths.append(path)
     for path in picture_paths:
-        pictures.append(Picture(path, width, height,len(picture_paths),len(pictures)+1))
+        if isfile(path):
+            pictures.append(Picture(path, width, height, len(picture_paths), len(pictures) + 1))
     return pictures
+
 
 def showPictures(display, font, pictures):
     running = True
@@ -114,16 +117,18 @@ def showPictures(display, font, pictures):
     else:
         pygame.quit()
 
+
 def main():
     args = parser.parse_args()
     pygame.init()
     pygame.display.set_caption("loading...")
-    display_width = 1920 
+    display_width = 1920
     display_height = 1080
     font = pygame.font.SysFont(None, 40)
-    display = pygame.display.set_mode((display_width,display_height))
+    display = pygame.display.set_mode((display_width, display_height))
     pictures = loadPictures(args.path, display_width, display_height)
     showPictures(display, font, pictures)
+
 
 if __name__ == "__main__":
     main()
